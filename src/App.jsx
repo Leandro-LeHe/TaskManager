c23a9b5f138df3cb123224259056c9c442e06219
leandroh <leandroh@defensoria.ms.def.br>
2025-09-17 14:12:22 -0400

    primeiro commit: configurando estrutura inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "learning math",
+      description: "LearningDev",
+      isCompleted: false,
+    },
+
+    {
+      id: "2",
+      title: "learning portuguese",
+      description: "LearningDev2",
+      isCompleted: false,
+    },
+
+    {
+      id: "3",
+      title: "learning english",
+      description: "LearningDev3",
+      isCompleted: false,
+    },
+  ]);
+  return (
+    <div className="w-screm h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Task Manager
+        </h1>
+        <Tasks tasks={tasks} />
+        <AddTask />
+      </div>
+    </div>
+  );
+}
+
+export default App;
