03b3d031415a2959b9911b95416648e91f6a1885
leandroh <leandroh@defensoria.ms.def.br>
2025-09-17 17:55:51 -0400

    atualizacao

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,8 +5,14 @@ function Tasks(props) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-left w-full text-white p-2 rounded shadow">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted && "line-through li"
+            }`}
+          >
             {task.title}
+            {task.isCompleted ? "COMPLETE" : "INCOMPLETE"}
           </button>
           <button className="bg-slate-400 p-2 rounded-nd text-white">
             <ChevronRightIcon />
